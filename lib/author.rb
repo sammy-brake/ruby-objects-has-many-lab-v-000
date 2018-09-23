36c8f01c77cb05795a6e565b2e00320cab8ab07d
Samantha brake <sammy.brake@gmail.com>
2018-09-23 20:46:58 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,8 +1,14 @@
 class Author 
   attr_accessor :name 
+  @@post_count = 0 
   
   def initialize(name)
     @name = name 
     @posts = []
+    @@post_count += 1 
+  end 
+  
+  def post_count 
+    @@post_count
   end 
 end 
