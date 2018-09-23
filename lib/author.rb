4fe844cb7d4e1581e236f29a4096c030a18ed10d
Samantha brake <sammy.brake@gmail.com>
2018-09-23 20:56:40 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,26 @@
+class Author 
+  attr_accessor :name 
+  attr_reader :posts 
+  @@post_count = 0 
+  
+  def initialize(name)
+    @name = name 
+    @posts = []
+   
+  end 
+  
+  def self.post_count 
+    @@post_count
+  end 
+  
+  def add_post(post)
+    @posts << post 
+    @@post_count += 1 
+    post.author = self 
+end 
+
+def add_post_by_title (title)
+  post = Post.new(title)
+  add_post(post)
+end 
+end 
